946f045ff41ad41734794c315ef2e37c3617dbed
massivebird <gdrakemail@gmail.com>
2023-02-18 01:02:20 -0500

    + p03 work

--- p03NameSaveDataProject/app/src/main/java/edu/wccnet/gadrake/p03_namesavedataproject/ui/main/MainFragment.kt
+++ p03NameSaveDataProject/app/src/main/java/edu/wccnet/gadrake/p03_namesavedataproject/ui/main/MainFragment.kt
@@ -7,8 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import edu.wccnet.gadrake.p03_namesavedataproject.R
+import edu.wccnet.gadrake.p03_namesavedataproject.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    // TODO: what is these for agane?
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -26,7 +31,29 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.namesText.text = viewModel.getNames()
+
+        binding.addNameButton.setOnClickListener {
+            if (binding.nameInput.text.isNotEmpty()) {
+                viewModel.addNameToList(binding.nameInput.text.toString())
+                binding.namesText.text = viewModel.getNames()
+            } else {
+                binding.namesText.text = "No Value"
+            }
+        }
     }
 
 }
--- p03NameSaveDataProject/app/src/main/java/edu/wccnet/gadrake/p03_namesavedataproject/ui/main/MainViewModel.kt
+++ p03NameSaveDataProject/app/src/main/java/edu/wccnet/gadrake/p03_namesavedataproject/ui/main/MainViewModel.kt
@@ -3,5 +3,13 @@ package edu.wccnet.gadrake.p03_namesavedataproject.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private lateinit var allNames: String
+
+    fun addNameToList(name: String) {
+
+    }
+
+    fun getNames() : String {
+        return allNames
+    }
 }
