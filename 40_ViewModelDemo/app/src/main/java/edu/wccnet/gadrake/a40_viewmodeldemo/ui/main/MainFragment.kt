ed7d3f5009011c18f302e77feb43908eb3c12e86
massivebird <gdrakemail@gmail.com>
2023-02-23 20:14:36 -0500

    + ch 40

--- 40_ViewModelDemo/app/src/main/java/edu/wccnet/gadrake/a40_viewmodeldemo/ui/main/MainFragment.kt
+++ 40_ViewModelDemo/app/src/main/java/edu/wccnet/gadrake/a40_viewmodeldemo/ui/main/MainFragment.kt
@@ -6,57 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import edu.wccnet.gadrake.a40_viewmodeldemo.R
 import edu.wccnet.gadrake.a40_viewmodeldemo.databinding.FragmentMainBinding
+import edu.wccnet.gadrake.a40_viewmodeldemo.BR.myViewModel
 
 class MainFragment : Fragment() {
-
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
-
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
-        // now handled by livedata;
-        // auto updates textview on UI recreation via lifecycle observation
-//        binding.resultText.text = viewModel.getResult().toString()
-
-        // ch 41 livedata
-        // implementing observer within UI controller (MainFragment.kt);
-        // lambda is passed current result value -> converts to string -> displays in textview
-        val resultObserver = Observer<Float> {
-                result -> binding.resultText.text = result.toString()
-        }
-        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
-
-        binding.convertButton.setOnClickListener {
-            if (binding.dollarText.text.isNotEmpty()) {
-                viewModel.setAmount(binding.dollarText.text.toString())
-            } else {
-                binding.resultText.text = "No value"
-            }
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding.setVariable(myViewModel, viewModel)
     }
 
 }
--- 40_ViewModelDemo/app/src/main/java/edu/wccnet/gadrake/a40_viewmodeldemo/ui/main/MainViewModel.kt
+++ 40_ViewModelDemo/app/src/main/java/edu/wccnet/gadrake/a40_viewmodeldemo/ui/main/MainViewModel.kt
@@ -6,18 +6,16 @@ import androidx.lifecycle.MutableLiveData
 
 class MainViewModel : ViewModel() {
     private val rate = 0.74f
-    private var dollarText = ""
-//    private var result: Float = 0f
-    private var result: MutableLiveData<Float> = MutableLiveData()
+    var dollarValue: MutableLiveData<String> = MutableLiveData()
+    var result: MutableLiveData<Float> = MutableLiveData()
 
-    fun setAmount(value: String) {
-        this.dollarText = value
-//        result = value.toFloat() * rate
-        result.value = value.toFloat() * rate
-    }
-
-//    fun getResult(): Float {
-    fun getResult(): MutableLiveData<Float> {
-        return result
+    fun convertValue() {
+        dollarValue.let {
+            if (!it.value.equals("")) {
+                result.value = it.value?.toFloat()?.times(rate)
+            } else {
+                result.value = 0f
+            }
+        }
     }
 }
